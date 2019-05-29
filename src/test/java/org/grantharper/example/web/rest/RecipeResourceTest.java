87a718c654022b26feb4cd0b5cb883b933e84901
grantharper <gharper3@gmail.com>
2019-05-29 17:59:59 -0400

    add test for recipe endpoint

---
+++ src/test/java/org/grantharper/example/web/rest/RecipeResourceTest.java
@@ -0,0 +1,34 @@
+package org.grantharper.example.web.rest;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class RecipeResourceTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void givenRequestAllRecipesShouldReturnAllRecipes() throws Exception {
+        mockMvc.perform(get("/recipe").accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isOk())
+               .andExpect(content().json("{'title':'Strawberry Shortcake','author':'Grant'}"));
+    }
+}
