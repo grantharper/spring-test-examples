23250a9cf56c3c1d024f51bbdb46fd8f7db36c22
grantharper <gharper3@gmail.com>
2019-05-29 21:11:46 -0400

    add @JsonProperty annotations to the json creator constructor for deserialization

--- src/main/java/org/grantharper/example/service/TranslationDTO.java
+++ src/main/java/org/grantharper/example/service/TranslationDTO.java
@@ -1,13 +1,16 @@
 package org.grantharper.example.service;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class TranslationDTO {
+
     private final String text;
     private final String lang;
 
     @JsonCreator
-    public TranslationDTO(String text, String lang) {
+    public TranslationDTO(@JsonProperty("text") String text,
+                          @JsonProperty("lang") String lang) {
         this.text = text;
         this.lang = lang;
     }
